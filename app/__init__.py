4d8fb962ed75ce8fa81d781004fdcd2c813ea808
Michael <deadlynighthawk99@gmail.com>
2016-05-12 22:31:09 -0400

    -a

--- app/__init__.py
+++ app/__init__.py
@@ -4,3 +4,5 @@ from config import basedir
 
 app = Flask(__name__)
 app.config.from_object('config')
+
+from app import views, models 
--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from flask import current_app, render_template
+from .models import *
 from app import app
 
 @app.route('/')
